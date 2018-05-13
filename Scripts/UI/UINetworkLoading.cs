06c6c0661efb9293341cb4edcffe0ec9e2ee1e31
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-05-13 14:40:13 +0700

    Add loading ui

---
+++ Scripts/UI/UINetworkLoading.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UINetworkLoading : MonoBehaviour
+{
+    public static UINetworkLoading Singleton { get; private set; }
+    public GameObject joiningLobbyObject;
+    public GameObject joiningRoomObject;
+
+    private void Awake()
+    {
+        if (Singleton != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+        Singleton = this;
+        SimplePhotonNetworkManager.onJoiningLobby += OnJoiningLobby;
+        SimplePhotonNetworkManager.onJoiningRoom += OnJoiningRoom;
+        SimplePhotonNetworkManager.onJoinedLobby += OnJoinedLobby;
+        SimplePhotonNetworkManager.onJoinedRoom += OnJoinedRoom;
+    }
+
+    public void OnJoiningLobby()
+    {
+        if (joiningLobbyObject != null)
+            joiningLobbyObject.SetActive(true);
+    }
+
+    public void OnJoiningRoom()
+    {
+        if (joiningRoomObject != null)
+            joiningRoomObject.SetActive(true);
+    }
+
+    public void OnJoinedLobby()
+    {
+        if (joiningLobbyObject != null)
+            joiningLobbyObject.SetActive(false);
+    }
+
+    public void OnJoinedRoom()
+    {
+        if (joiningRoomObject != null)
+            joiningRoomObject.SetActive(false);
+    }
+}
