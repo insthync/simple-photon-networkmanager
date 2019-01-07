2e0bccee753e45e80cf1c1858d0bb8cec74d9ae4
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-01-07 22:36:14 +0700

    WIP Waiting room

---
+++ Scripts/UI/UIPhotonWaitingRoom.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIPhotonWaitingRoom : UIPhotonGameCreate
+{
+    public override void Show()
+    {
+        base.Show();
+        SimplePhotonNetworkManager.onJoinedRoom -= OnJoinedRoom;
+    }
+
+    public virtual void OnClickCreateWaitingRoom()
+    {
+        SimplePhotonNetworkManager.Singleton.CreateWaitingRoom();
+        SimplePhotonNetworkManager.onJoinedRoom += OnJoinedRoom;
+    }
+
+    public override void OnClickCreateGame()
+    {
+        SimplePhotonNetworkManager.Singleton.StartGame();
+    }
+
+    private void OnJoinedRoom()
+    {
+        Show();
+    }
+}
