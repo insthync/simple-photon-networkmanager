b58bdd520568741b5d05b3aacbd0b09d58859dc1
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-01-08 15:44:18 +0700

    WIP waiting room system

--- Scripts/UI/UIPhotonWaitingPlayer.cs
+++ Scripts/UI/UIPhotonWaitingPlayer.cs
@@ -1,16 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class UIPhotonWaitingPlayer : MonoBehaviour {
+public class UIPhotonWaitingPlayer : MonoBehaviour
+{
+    public Text textPlayerName;
+    public Text textPlayerState;
+    public GameObject isLeaderObject;
+    public string playerStateReady = "Ready";
+    public string playerStateNotReady = "Not Ready";
+    public GameObject[] owningObjects;
+    private UIPhotonWaitingRoom _room;
+    private PhotonPlayer _data;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public void SetData(UIPhotonWaitingRoom room, PhotonPlayer data)
+    {
+        _room = room;
+        _data = data;
+        var state = (byte)data.CustomProperties[SimplePhotonNetworkManager.CUSTOM_PLAYER_STATE];
+
+        if (textPlayerName != null)
+            textPlayerName.text = data.NickName;
+
+        if (textPlayerState != null)
+        {
+            switch ((SimplePhotonNetworkManager.PlayerState)state)
+            {
+                case SimplePhotonNetworkManager.PlayerState.Ready:
+                    textPlayerState.text = playerStateReady;
+                    break;
+                case SimplePhotonNetworkManager.PlayerState.NotReady:
+                    textPlayerState.text = playerStateNotReady;
+                    break;
+            }
+        }
+
+        if (isLeaderObject != null)
+            isLeaderObject.SetActive(room.HostPlayerID == data.ID);
+
+        foreach (var owningObject in owningObjects)
+        {
+            owningObject.SetActive(PhotonNetwork.player.ID == data.ID);
+        }
+    }
 }
--- Scripts/UI/UIPhotonWaitingRoom.cs
+++ Scripts/UI/UIPhotonWaitingRoom.cs
@@ -13,23 +13,39 @@ public class UIPhotonWaitingRoom : UIBase
     public Text textRoomState;
     public string roomStateWaiting = "Waiting";
     public string roomStatePlaying = "Playing";
+    public Text textGameRule;
+    public Text textBotCount;
+    public Text textMatchTime;
+    public Text textMatchKill;
+    public Text textMatchScore;
+    public UIPhotonWaitingPlayer waitingPlayerPrefab;
+    public Transform waitingPlayerListContainer;
+    public Transform waitingPlayerTeamAListContainer;
+    public Transform waitingPlayerTeamBListContainer;
+    public GameObject[] hostObjects;
+    public GameObject[] nonHostObjects;
+    public int HostPlayerID { get; private set; }
+
+    private readonly Dictionary<int, UIPhotonWaitingPlayer> waitingPlayers = new Dictionary<int, UIPhotonWaitingPlayer>();
+    private readonly Dictionary<int, UIPhotonWaitingPlayer> waitingTeamAPlayers = new Dictionary<int, UIPhotonWaitingPlayer>();
+    private readonly Dictionary<int, UIPhotonWaitingPlayer> waitingTeamBPlayers = new Dictionary<int, UIPhotonWaitingPlayer>();
 
     private void OnEnable()
     {
-        SimplePhotonNetworkManager.onJoinedRoom += OnJoinedRoom;
-        SimplePhotonNetworkManager.onPlayerConnected += OnPlayerConnected;
-        SimplePhotonNetworkManager.onPlayerDisconnected += OnPlayerDisconnected;
-        SimplePhotonNetworkManager.onPlayerPropertiesChanged += OnPlayerPropertiesChanged;
-        SimplePhotonNetworkManager.onCustomRoomPropertiesChanged += OnCustomRoomPropertiesChanged;
+        SimplePhotonNetworkManager.onJoinedRoom += OnJoinedRoomCallback;
+        SimplePhotonNetworkManager.onPlayerConnected += OnPlayerConnectedCallback;
+        SimplePhotonNetworkManager.onPlayerDisconnected += OnPlayerDisconnectedCallback;
+        SimplePhotonNetworkManager.onPlayerPropertiesChanged += OnPlayerPropertiesChangedCallback;
+        SimplePhotonNetworkManager.onCustomRoomPropertiesChanged += OnCustomRoomPropertiesChangedCallback;
     }
 
     private void OnDisable()
     {
-        SimplePhotonNetworkManager.onJoinedRoom -= OnJoinedRoom;
-        SimplePhotonNetworkManager.onPlayerConnected -= OnPlayerConnected;
-        SimplePhotonNetworkManager.onPlayerDisconnected -= OnPlayerDisconnected;
-        SimplePhotonNetworkManager.onPlayerPropertiesChanged -= OnPlayerPropertiesChanged;
-        SimplePhotonNetworkManager.onCustomRoomPropertiesChanged -= OnCustomRoomPropertiesChanged;
+        SimplePhotonNetworkManager.onJoinedRoom -= OnJoinedRoomCallback;
+        SimplePhotonNetworkManager.onPlayerConnected -= OnPlayerConnectedCallback;
+        SimplePhotonNetworkManager.onPlayerDisconnected -= OnPlayerDisconnectedCallback;
+        SimplePhotonNetworkManager.onPlayerPropertiesChanged -= OnPlayerPropertiesChangedCallback;
+        SimplePhotonNetworkManager.onCustomRoomPropertiesChanged -= OnCustomRoomPropertiesChangedCallback;
     }
 
     private void UpdateRoomData()
@@ -62,6 +78,31 @@ public class UIPhotonWaitingRoom : UIBase
                     break;
             }
         }
+
+        object gameRuleObject;
+        BaseNetworkGameRule gameRule;
+        if (textGameRule != null &&
+            customProperties.TryGetValue(BaseNetworkGameManager.CUSTOM_ROOM_GAME_RULE, out gameRuleObject) &&
+            BaseNetworkGameInstance.GameRules.TryGetValue(gameRuleObject.ToString(), out gameRule))
+            textGameRule.text = gameRule == null ? "" : gameRule.Title;
+
+        object botCountObject;
+        if (textBotCount != null && customProperties.TryGetValue(BaseNetworkGameManager.CUSTOM_ROOM_GAME_RULE_BOT_COUNT, out botCountObject))
+            textBotCount.text = ((int)botCountObject).ToString("N0");
+
+        object matchTimeObject;
+        if (textMatchTime != null && customProperties.TryGetValue(BaseNetworkGameManager.CUSTOM_ROOM_GAME_RULE_MATCH_TIME, out matchTimeObject))
+            textMatchTime.text = ((int)matchTimeObject).ToString("N0");
+
+        object matchKillObject;
+        if (textMatchKill != null && customProperties.TryGetValue(BaseNetworkGameManager.CUSTOM_ROOM_GAME_RULE_MATCH_KILL, out matchKillObject))
+            textMatchKill.text = ((int)matchKillObject).ToString("N0");
+
+        object matchScoreObject;
+        if (textMatchScore != null && customProperties.TryGetValue(BaseNetworkGameManager.CUSTOM_ROOM_GAME_RULE_MATCH_SCORE, out matchScoreObject))
+            textMatchScore.text = ((int)matchScoreObject).ToString("N0");
+
+        HostPlayerID = playerId;
     }
 
     public virtual void OnClickLeaveRoom()
@@ -74,27 +115,104 @@ public class UIPhotonWaitingRoom : UIBase
         SimplePhotonNetworkManager.Singleton.StartGame();
     }
 
-    private void OnJoinedRoom()
+    public virtual void OnClickReady()
+    {
+        Hashtable customProperties = PhotonNetwork.player.CustomProperties;
+        SimplePhotonNetworkManager.PlayerState state = SimplePhotonNetworkManager.PlayerState.NotReady;
+        object stateObj;
+        if (customProperties.TryGetValue(SimplePhotonNetworkManager.CUSTOM_PLAYER_STATE, out stateObj))
+            state = (SimplePhotonNetworkManager.PlayerState) (byte) stateObj;
+        // Toggle state
+        if (state == SimplePhotonNetworkManager.PlayerState.NotReady)
+            state = SimplePhotonNetworkManager.PlayerState.Ready;
+        if (state == SimplePhotonNetworkManager.PlayerState.Ready)
+            state = SimplePhotonNetworkManager.PlayerState.NotReady;
+        // Set state property
+        customProperties[SimplePhotonNetworkManager.CUSTOM_PLAYER_STATE] = (byte)state;
+        PhotonNetwork.player.SetCustomProperties(customProperties);
+    }
+
+    private void OnJoinedRoomCallback()
     {
         UpdateRoomData();
+        // Set waiting player list
+        for (var i = waitingPlayerListContainer.childCount - 1; i >= 0; --i)
+        {
+            var child = waitingPlayerListContainer.GetChild(i);
+            Destroy(child.gameObject);
+        }
+
+        for (var i = waitingPlayerTeamAListContainer.childCount - 1; i >= 0; --i)
+        {
+            var child = waitingPlayerTeamAListContainer.GetChild(i);
+            Destroy(child.gameObject);
+        }
+
+        for (var i = waitingPlayerTeamBListContainer.childCount - 1; i >= 0; --i)
+        {
+            var child = waitingPlayerTeamBListContainer.GetChild(i);
+            Destroy(child.gameObject);
+        }
+
+        waitingPlayers.Clear();
+        foreach (PhotonPlayer data in PhotonNetwork.playerList)
+        {
+            int key = data.ID;
+            if (!waitingPlayers.ContainsKey(key))
+            {
+                var newEntry = Instantiate(waitingPlayerPrefab, waitingPlayerListContainer);
+                newEntry.SetData(this, data);
+                newEntry.gameObject.SetActive(true);
+                waitingPlayers.Add(key, newEntry);
+            }
+        }
+        foreach (var hostObject in hostObjects)
+        {
+            hostObject.SetActive(HostPlayerID == PhotonNetwork.player.ID);
+        }
+        foreach (var nonHostObject in nonHostObjects)
+        {
+            nonHostObject.SetActive(HostPlayerID != PhotonNetwork.player.ID);
+        }
     }
 
-    private void OnPlayerConnected(PhotonPlayer player)
+    private void OnPlayerConnectedCallback(PhotonPlayer player)
     {
         UpdateRoomData();
+        int key = player.ID;
+        if (waitingPlayers.ContainsKey(key))
+        {
+            Destroy(waitingPlayers[key].gameObject);
+            waitingPlayers.Remove(key);
+        }
+
+        var newEntry = Instantiate(waitingPlayerPrefab, waitingPlayerListContainer);
+        newEntry.SetData(this, player);
+        newEntry.gameObject.SetActive(true);
+        waitingPlayers.Add(key, newEntry);
     }
 
-    private void OnPlayerDisconnected(PhotonPlayer player)
+    private void OnPlayerDisconnectedCallback(PhotonPlayer player)
     {
         UpdateRoomData();
+        int key = player.ID;
+        if (waitingPlayers.ContainsKey(key))
+        {
+            Destroy(waitingPlayers[key].gameObject);
+            waitingPlayers.Remove(key);
+        }
     }
 
-    private void OnPlayerPropertiesChanged(object[] playerAndUpdatedProps)
+    private void OnPlayerPropertiesChangedCallback(object[] playerAndUpdatedProps)
     {
-
+        // TODO: Under testing
+        foreach (var playerAndUpdatedProp in playerAndUpdatedProps)
+        {
+            Debug.LogError(playerAndUpdatedProp.ToString());
+        }
     }
 
-    private void OnCustomRoomPropertiesChanged(Hashtable propertiesThatChanged)
+    private void OnCustomRoomPropertiesChangedCallback(Hashtable propertiesThatChanged)
     {
         UpdateRoomData();
     }
