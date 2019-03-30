f03325556fe243baee4318e7753383536cb422aa
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-03-30 14:58:30 +0700

    Add online object activator

---
+++ Scripts/PhotonOnlineObjectActivator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotonOnlineObjectActivator : MonoBehaviour
+{
+    public GameObject[] onlineObjects;
+    public GameObject[] offlineObjects;
+    
+    void Update()
+    {
+        foreach (var obj in onlineObjects)
+        {
+            obj.SetActive(!PhotonNetwork.offlineMode);
+        }
+        foreach (var obj in offlineObjects)
+        {
+            obj.SetActive(PhotonNetwork.offlineMode);
+        }
+    }
+}
